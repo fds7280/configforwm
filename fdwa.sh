aa3a86f23db1b1d15c1d4fee488217652ae327ec
fds7280 <bludfree0@gmail.com>
2025-02-26 17:39:25 +0530

    Add files via upload

---
+++ fdwa.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the directory path
+directory="/home/bear/Downloads/wps"
+
+# Loop through all image files in the directory
+for file in "$directory"/*.jpg "$directory"/*.png "$directory"/*.gif; do
+    # Extract the filename without path
+    filename="${file##*/}"
+    # Use the filename as the display text and the file path as the icon
+    echo -en "${filename}\0icon\x1fthumbnail://${file}\n"
+done | rofi -dmenu -show-icons -p "Select Image:" | while read selected_image; do
+    # Execute the command with the selected image path
+    swww img "/home/bear/Downloads/wps/${selected_image}"
+done
+
